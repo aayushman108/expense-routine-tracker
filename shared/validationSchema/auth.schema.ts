1d56524ead464b3f9a47ffe665d3f92927bdd4fd
Aayushman Sharma <aayushmansharma108@gmail.com>
2026-02-17 05:30:50 +0545

    feat: add shape.body in auth schema to comply with validationSchema

--- shared/validationSchema/auth.schema.ts
+++ shared/validationSchema/auth.schema.ts
@@ -7,65 +7,71 @@ import {
 
 export class UserValidation {
   static signupSchema = z.object({
-    fullName: z.preprocess(
-      requiredPreprocessor,
-      z
-        .string({ message: "Full name is required" })
-        .min(1, { message: "Full name is required" })
-        .max(255, { message: "Full name must not exceed 255 characters" }),
-    ),
+    body: z.object({
+      fullName: z.preprocess(
+        requiredPreprocessor,
+        z
+          .string({ message: "Full name is required" })
+          .min(1, { message: "Full name is required" })
+          .max(255, { message: "Full name must not exceed 255 characters" }),
+      ),
 
-    email: z.preprocess(
-      emailPreprocessor,
-      z
-        .string({ message: "Email is required" })
-        .email({ message: "Invalid email format" })
-        .max(255, { message: "Email must not exceed 255 characters" }),
-    ),
-    phone: z.preprocess(
-      optionalPreprocessor,
-      z
-        .string()
-        .max(20, { message: "Phone number must not exceed 20 characters" })
-        .nullable(),
-    ),
-    password: z.preprocess(
-      requiredPreprocessor,
-      z
-        .string({ message: "Password is required" })
-        .min(8, { message: "Password must be at least 8 characters" })
-        .regex(
-          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/,
-          {
-            message:
-              "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character (@$!%*?&#)",
-          },
-        ),
-    ),
+      email: z.preprocess(
+        emailPreprocessor,
+        z
+          .string({ message: "Email is required" })
+          .email({ message: "Invalid email format" })
+          .max(255, { message: "Email must not exceed 255 characters" }),
+      ),
+      phone: z.preprocess(
+        optionalPreprocessor,
+        z
+          .string()
+          .max(20, { message: "Phone number must not exceed 20 characters" })
+          .nullable(),
+      ),
+      password: z.preprocess(
+        requiredPreprocessor,
+        z
+          .string({ message: "Password is required" })
+          .min(8, { message: "Password must be at least 8 characters" })
+          .regex(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/,
+            {
+              message:
+                "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character (@$!%*?&#)",
+            },
+          ),
+      ),
+    }),
   });
 
   static loginSchema = z.object({
-    email: z.preprocess(
-      emailPreprocessor,
-      z
-        .string({ message: "Email is required" })
-        .email({ message: "Invalid email format" }),
-    ),
-    password: z.preprocess(
-      requiredPreprocessor,
-      z
-        .string({ message: "Password is required" })
-        .min(8, { message: "Password must be at least 8 characters" })
-        .regex(
-          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/,
-          {
-            message:
-              "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character (@$!%*?&#)",
-          },
-        ),
-    ),
+    body: z.object({
+      email: z.preprocess(
+        emailPreprocessor,
+        z
+          .string({ message: "Email is required" })
+          .email({ message: "Invalid email format" }),
+      ),
+      password: z.preprocess(
+        requiredPreprocessor,
+        z
+          .string({ message: "Password is required" })
+          .min(8, { message: "Password must be at least 8 characters" })
+          .regex(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/,
+            {
+              message:
+                "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character (@$!%*?&#)",
+            },
+          ),
+      ),
+    }),
   });
 }
 
-export type ISignupInput = z.infer<typeof UserValidation.signupSchema>;
-export type ILoginInput = z.infer<typeof UserValidation.loginSchema>;
+export type ISignupInput = z.infer<
+  typeof UserValidation.signupSchema.shape.body
+>;
+export type ILoginInput = z.infer<typeof UserValidation.loginSchema.shape.body>;
