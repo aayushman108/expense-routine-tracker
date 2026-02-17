586476aaefb0a3f40dac04d7d28aa788db2d10a9
Aayushman Sharma <aayushmansharma108@gmail.com>
2026-02-17 06:38:17 +0545

    feat: add enum directory in shared directory

---
+++ shared/enum/general.enum.ts
@@ -0,0 +1,13 @@
+export enum SUPPORTED_CURRENCIES {
+  NPR = "NPR",
+  USD = "USD",
+  EUR = "EUR",
+  GBP = "GBP",
+  INR = "INR",
+}
+
+export enum SPLIT_MODE {
+  EQUAL = "equal",
+  PERCENTAGE = "percentage",
+  AMOUNT = "amount",
+}
