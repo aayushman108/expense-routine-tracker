c41528f857e5e28df3c1c58a0ddac35de5c82e6e
Aayushman Sharma <aayushmansharma108@gmail.com>
2026-02-22 17:52:18 +0545

    feat: change db_schema for settlements and expense_split

--- backend/src/database/schema.sql
+++ backend/src/database/schema.sql
@@ -1,4 +1,3 @@
-
 -- Users table
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -11,8 +10,7 @@ CREATE TABLE users (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-
--- GROUPS
+-- Groups table
 CREATE TABLE groups (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(255) NOT NULL,
@@ -20,10 +18,11 @@ CREATE TABLE groups (
     image JSONB,
     created_by UUID NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (created_by) REFERENCES users(id)
 );
 
--- GROUP MEMBERS
+-- Group Members table
 CREATE TABLE group_members (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     group_id UUID NOT NULL,
@@ -31,13 +30,13 @@ CREATE TABLE group_members (
     nickname VARCHAR(100),
     role VARCHAR(50) DEFAULT 'member',
     joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    left_at TIMESTAMP NULL, -- NULL means currently a member
+    left_at TIMESTAMP NULL,
     FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE RESTRICT,
     CONSTRAINT unique_group_user UNIQUE (group_id, user_id)
 );
 
--- EXPENSES
+-- Expenses table
 CREATE TABLE expenses (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     group_id UUID NULL, -- NULL for personal expenses
@@ -47,43 +46,56 @@ CREATE TABLE expenses (
     expense_date DATE NOT NULL,
     currency VARCHAR(3) DEFAULT 'NPR',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE,
     FOREIGN KEY (paid_by) REFERENCES users(id) ON DELETE RESTRICT
 );
 
--- EXPENSE SPLITS
+-- Expense Splits table
 CREATE TABLE expense_splits (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     expense_id UUID NOT NULL,
     user_id UUID NOT NULL,
     split_percentage DECIMAL(5,2) NOT NULL CHECK (split_percentage >= 0 AND split_percentage <= 100),
     split_amount DECIMAL(12,2) NOT NULL CHECK (split_amount >= 0),
+    settlement_id UUID NULL,
     FOREIGN KEY (expense_id) REFERENCES expenses(id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE RESTRICT,
+    FOREIGN KEY (settlement_id) REFERENCES settlements(id) ON DELETE SET NULL,
+
     CONSTRAINT unique_expense_user UNIQUE (expense_id, user_id)
 );
 
+-- Payment Methods table
 CREATE TABLE payment_methods (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID NOT NULL,
-    provider VARCHAR(100) NOT NULL, -- e.g. khalti, esewa, bank
+    provider VARCHAR(100) NOT NULL,
     external_id VARCHAR(255),
     metadata JSONB,
     is_verified BOOLEAN DEFAULT FALSE,
     is_default BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
--- MONTHLY SETTLEMENTS
+-- Settlements table
 CREATE TABLE settlements (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    expense_split_id UUID NOT NULL,
-    status VARCHAR(10) DEFAULT 'pending', -- pending / paid
-    proof_image JSONB, -- stores payment screenshot details
+    group_id UUID NOT NULL,
+    from_user_id UUID NOT NULL,
+    to_user_id UUID NOT NULL,
+    amount DECIMAL(12,2) NOT NULL CHECK (amount <> 0), -- amount can be positive or negative. if positive, from_user_id owes to_user_id. if negative, to_user_id owes from_user_id.
+    status VARCHAR(20) DEFAULT 'pending' CHECK (status IN ('pending','paid','confirmed','rejected')), -- pending(by default), paid(by from_user_id), confirmed(by to_user_id/from_user_id) or rejected(by to_user_id/from_user_id), but the amount sign decides who owes whom.
+    proof_image JSONB NULL,
+    paid_at TIMESTAMP NULL,
+    reviewed_at TIMESTAMP NULL,
+    reviewed_by UUID NULL, -- will be to_user_id if amount is +ve or from_user_id if amount is -ve
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (expense_split_id) REFERENCES expense_splits(id) ON DELETE CASCADE
+    FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE,
+    FOREIGN KEY (from_user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (to_user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (reviewed_by) REFERENCES users(id) ON DELETE SET NULL
 );
-
