a7beaa29be451b3a5d95afc128408273f25a4829
Aayushman Sharma <aayushmansharma108@gmail.com>
2026-02-27 09:56:23 +0545

    feat: add EXPENSE_STATUS and SPLIT_STATUS enum, and add basic expense process flow

--- shared/enum/general.enum.ts
+++ shared/enum/general.enum.ts
@@ -16,3 +16,25 @@ export enum EXPENSE_TYPE {
   PERSONAL = "personal",
   GROUP = "group",
 }
+
+export enum EXPENSE_STATUS {
+  DRAFT = "draft",
+  SUBMITTED = "submitted",
+  VERIFIED = "verified",
+  REJECTED = "rejected",
+}
+
+export enum SPLIT_STATUS {
+  PENDING = "pending",
+  VERIFIED = "verified",
+  REJECTED = "rejected",
+}
+
+/**
+ * @description
+ * if all split_status are verified then expense_status will be verified,
+ * if one or more split_status are rejected then expense_status will be rejected,
+ * if all split_status are mixed then expense_status will be submitted,
+ * expense_status will be draft if the expense is not submitted yet,
+ * if expense_status is verified then send email to all the members included in the split,
+ */
